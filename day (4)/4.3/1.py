101960cf4cc749891ffa6c1381c8dda9db5f4b46
sms_solution <sms.hardware.solution@gmail.com>
2024-05-24 16:21:15 +0600

    Add files via upload

---
+++ day (4)/4.3/1.py
@@ -0,0 +1,9 @@
+mark_in_math=int(input())
+mark_in_english=int(input())
+
+if mark_in_math >= 80:
+  print("prize! for math")
+elif mark_in_english>=80:
+  print("prize!for english")
+else :
+  print("no prize")
---
+++ day (4)/4.3/3.py
@@ -0,0 +1,9 @@
+year=int(input("enter a year:"))
+if year%400==0:
+  print(f"{year}is a leap year.")
+elif year%100==0:
+  print(f"{year} is a not leap year.")
+elif year%4==0:
+  print(f"{year} is a leap year.")
+else:
+  print(f"{year}is not a leap year.")
